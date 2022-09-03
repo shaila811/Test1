ca2cfee6a6a88d94a876990b217e42c7640f019e
shaila811 <shaila11bd@gmail.com>
2022-09-03 00:27:52 -0400

    Bootstrap

--- src/main/java/com/TestingB1/Bootstrap.java
+++ src/main/java/com/TestingB1/Bootstrap.java
@@ -1,10 +1,40 @@
 package com.TestingB1;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Bootstrap {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(6000l, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3000l, TimeUnit.SECONDS);
+		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
+		driver.findElement(By.xpath("//button[@type='button']")).click();
+		List<WebElement> list = driver.findElements(By.xpath("//ul[@class='multiselect-container dropdown-menu']//li"));
+		
+for(int i=0;i<list.size();i++) {		
+System.out.println(list.get(i).getText());}
+for(int i=0;i<list.size();i++)
+if (list.get(i).getAttribute("class").equalsIgnoreCase("active")) {
+System.out.println(list.get(i).getText());
+}else {
 
+list.get(i).click();
+}
+Thread.sleep(3000);
+driver.quit();		
+		
 	}
 
 }
